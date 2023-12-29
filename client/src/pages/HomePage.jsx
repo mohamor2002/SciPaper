4cb186b942fe0c328595f4021f2332e28b76c29a
mohamor2002 <muhammadamor2002@gmail.com>
2023-12-29 19:36:40 +0100

    home page added

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,8 +1,11 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { motion,AnimatePresence } from 'framer-motion'
 import LogoAnimation from '../components/LogoAnimation'
 import MailOutlineOutlinedIcon from '@mui/icons-material/MailOutlineOutlined';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
+import bg1 from '../assets/bg-1.png'
+import bg2 from '../assets/bg-2.png'
 import {Link} from 'react-router-dom'
 
 const HomePage = () => {
@@ -23,13 +26,39 @@ const HomePage = () => {
         duration: 0.5,
       };
 
+      const keyframes = {
+        start: {
+          opacity: 0.4,
+          x: 0,
+          scale: 0.9,
+        },
+        end: {
+          opacity: 1,
+          x: 0,
+          scale: 1,
+        },
+      };
+    
+      const transition = {
+        duration: 3,
+        ease: 'easeInOut',
+        repeat:Infinity,
+        repeatType: 'reverse', // Reverse the animation on each repeat
+      };
+    
+
+
+      useEffect(()=>{
+        setCurrentText(isLogin?'Sign up and discover a great amount of new opportunities!':'To keep connected with us please login with you personal info')
+        setCurrentTitle(isLogin?'New Here?':'Welcome back!')
+        setCurrentButton(!isLogin?'Sign In':'Sign Up')
+  
+      },[isLogin])
       const handleToggle=(e)=>{
             e.preventDefault()
             setIsLogin(prev=>!prev)
-            setCurrentText(isLogin?'Sign up and discover a great amount of new opportunities!':'To keep connected with us please login with you personal info')
-            setCurrentTitle(isLogin?'New Here?':'Welcome back!')
-            setCurrentButton(!isLogin?'Sign In':'Sign Up')
-      }
+        }
+            
 
       
 
@@ -38,17 +67,25 @@ const HomePage = () => {
 
     return (
     <div className={`relative font-br-hendrix h-screen w-full flex ${isLogin?'md:flex-row flex-col':'md:flex-row-reverse flex-col-reverse'}`}>
-        <div className=' absolute z-10 left-2 top-4'>
+        <div className=' absolute z-10 left-2 top-4 flex items-center'>
             {
                 isLogin?
                 <LogoAnimation colors={colorsMain}/>:
                 <LogoAnimation colors={colorsWhite}/>
             }
+            <p className={`${isLogin?'text-dark-blue':'text-white'} font-bold text-2xl duration-1000`}>SciPaper</p>
         </div>
-        <motion.div layout className=' flex-1 flex flex-col items-center justify-between'>
-            <div className=' h-[20%]'></div>
-            <h1 className='font-black text-dark-purple text-6xl'>Login to Your Account </h1>
+        <motion.div layout transition={{duration:1}} className=' flex-1 flex flex-col items-center justify-between'>
+            
+            <div className={` ${!isLogin?'hidden md:flex h-[15%]':'flex h-[20%]'} `}></div>
+            <h1 className={`font-black text-dark-purple text-5xl md:text-6xl text-center ${!isLogin&&'mt-8 md:mt-0'}`}>{isLogin?'Login to Your Account':'Create Account'} </h1>
             <p className=' text-[#414141] opacity-50 text-xl'>Login to use our service</p>
+            {
+                !isLogin&&
+                <div className=' w-[70%] h-[10%] bg-main-gray rounded-full flex items-center justify-between space-x-4 px-4'>
+                <PersonOutlinedIcon style={{color:'#352F44',fontSize:30}}/>
+                <input placeholder='Full Name' type="text" className=' text-lg font-medium bg-transparent outline-none flex-1' />
+            </div>}
             <div className=' w-[70%] h-[10%] bg-main-gray rounded-full flex items-center justify-between space-x-4 px-4'>
                 <MailOutlineOutlinedIcon style={{color:'#352F44',fontSize:30}}/>
                 <input placeholder='Email' type="email" className=' text-lg font-medium bg-transparent outline-none flex-1' />
@@ -58,22 +95,34 @@ const HomePage = () => {
                 <LockOutlinedIcon style={{color:'#352F44',fontSize:30}}/>
                 <input placeholder='Password' type="password" className=' text-lg font-medium bg-transparent outline-none flex-1' />
             </div>
-            <div className='flex w-[70%] justify-start font-semibold text-lg text-dark-purple'>
-                <Link className=' '>
-                    Forgot your password?
-                </Link>
+            <div className='flex mt-2 w-[70%] justify-start font-semibold md:text-lg text-dark-purple'>
+                {
+                    isLogin?
+                    <Link className=' '>
+                        Forgot your password?
+                    </Link>:
+                    <div className='flex space-x-4'>
+                        <input id='check' type="checkbox" className=' scale-150 accent-dark-purple ' />
+                        <label htmlFor="check">I have read the Term & Conditions</label>
+                    </div>
+                }
             </div>
             <button className='w-[33%] mb-2 h-16 bg-dark-purple rounded-full text-white font-bold text-xl'>
-                <p>Sign in</p>
+                <p>{isLogin?'Sign in':'Sign up'}</p>
             </button>
+            <div className='flex justify-center items-center mb-4 space-x-4 text-[#696969] text-sm'>
+                <p>Copyright@SciPaper</p>
+                <p>All rights reserved</p>
+            </div>
 
 
         </motion.div>
-        <motion.div layout transition={{duration:1}} className={`relative md:w-[40%] md:h-auto h-[40%] bg-gradient-to-tr from-light-purple via-main-purple to-secondary-purple flex flex-col justify-center items-center`}>
-            <div className=' flex flex-col items-center w-full space-y-6'>
+        <motion.div layout transition={{duration:1}} className={`relative md:w-[40%] md:h-auto h-[40%] bg-gradient-to-tr from-light-purple via-main-purple to-secondary-purple flex flex-col ${!isLogin?'md:justify-center md:pb-0 justify-end pb-10':'justify-center'}  items-center`}>
+            <motion.img initial="start" animate="end" variants={keyframes} transition={transition} src={isLogin?bg2:bg1} className='absolute h-full' alt="" />
+            <div className='z-10 flex flex-col items-center w-full space-y-6'>
                 <AnimatePresence exitBeforeEnter={false} mode="wait">
                     {
-                        <motion.div key={currentTitle} initial="initial" animate="animate" exit="exit" variants={textVariants} transition={textTransition}  className=' font-black text-white text-5xl'>
+                        <motion.div key={currentTitle} initial="initial" animate="animate" exit="exit" variants={textVariants} transition={textTransition}  className={`font-black text-white text-4xl md:text-5xl`}>
                             <h2>
                                 {currentTitle}
                             </h2>
