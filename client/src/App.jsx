d414c72df5eb4393d677339688fa94588ee2edb5
mohamor2002 <muhammadamor2002@gmail.com>
2023-12-30 14:49:06 +0100

    added screen resize

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
@@ -6,7 +6,34 @@ import HomePage from './pages/HomePage';
 
 function App() {
   
+  const [isKeyboardOpen, setIsKeyboardOpen] = useState(false);
 
+  useEffect(() => {
+    const handleResize = () => {
+      // Calculate the difference between the initial height and the new height
+      const heightDifference = window.innerHeight - document.documentElement.clientHeight;
+
+      // Check if the height difference is positive (keyboard opened)
+      setIsKeyboardOpen(heightDifference > 0);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    // If the keyboard is open, you can scroll to a specific element or position
+    if (isKeyboardOpen) {
+      // You can adjust this value based on your design
+      const scrollAmount = 100;
+
+      // Scroll to a specific position or element
+      window.scrollBy(0, scrollAmount);
+    }
+  }, [isKeyboardOpen]);
   return (
     <>
       <BrowserRouter>
